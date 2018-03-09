b3c44abf7a220b29b2263162d9fc15f760cfa44e
Grayson <info@fromconcentratesoftware.com>
2018-03-08 21:46:54 -0500

    Replace exploratory code with wrapper

--- library/deductionlib/deductionlib.cpp
+++ library/deductionlib/deductionlib.cpp
@@ -12,74 +12,15 @@
 #include <vector>
 #include <numeric>
 
+#include "cursor.hpp"
+
 namespace {
-	std::string qualify_name(CXCursor cursor) {
-		auto canContinue = [](CXCursor const & tmp) {
-			auto kind = clang_getCursorKind(tmp);
-			return !clang_Cursor_isNull(tmp)
-				&& !clang_isInvalid(kind)
-				&& kind != CXCursor_TranslationUnit;
-		};
-
-		auto components = std::vector<std::string> {};
-		while(canContinue(cursor)) {
-			auto cursorSpelling = clang_getCursorSpelling(cursor);
-			components.emplace_back(std::string { clang_getCString(cursorSpelling) });
-			clang_disposeString(cursorSpelling);
-			cursor = clang_getCursorSemanticParent(cursor);
-		}
-
-		auto begin = components.begin();
-		auto seed = *begin;
-		begin++;
-		std::string name = std::accumulate(begin, components.end(), seed, [](auto const & accumulated, auto const & next) {
-			return next + "::" + accumulated;
-		});
-		return name;
-	}
-
 	CXChildVisitResult visit(CXCursor cursor, CXCursor parent, CXClientData client_data) {
-		if (clang_getCursorVisibility(cursor) != CXVisibility_Default)
-			return CXChildVisit_Continue;
-		if (clang_getCursorAvailability(cursor) != CXAvailability_Available)
-			return CXChildVisit_Continue;
-
-		auto location = clang_getCursorLocation(cursor);
-		if (!clang_Location_isFromMainFile(location))
+		auto wrapper = deduction::cursor { cursor };
+		if (wrapper.is_invalid() || !wrapper.is_visible() || !wrapper.is_available() || !wrapper.is_in_main_file() || !wrapper.is_preprocessing())
 			return CXChildVisit_Continue;
 
-		auto kind = clang_getCursorKind(cursor);
-
-		if (clang_isPreprocessing(kind)) {
-			std::cout << "preprocessing" << std::endl;
-			return CXChildVisit_Continue;
-		}
-
-		auto availability = clang_getCursorAvailability(cursor);
-		auto language = clang_getCursorLanguage(cursor);
-		auto includedFile = clang_getIncludedFile(cursor);
-		auto type = clang_getCursorType(cursor);
-		auto typeSpelling = clang_getTypeSpelling(type);
-		auto underlyingTypedef = clang_getTypedefDeclUnderlyingType(cursor);
-		auto spelling = clang_getCursorSpelling(cursor);
-
-		auto usr = clang_getCursorUSR(cursor);
-		auto displayName = clang_getCursorDisplayName(cursor);
-
-		std::cout << clang_getCString(spelling)
-			<< " of type "
-			<< clang_getCString(typeSpelling)
-			<< " with usr "
-			<< clang_getCString(usr)
-			<< " and display name "
-			<< clang_getCString(displayName)
-			<< " fqdn: "
-			<< qualify_name(cursor)
-			<< std::endl;
-		clang_disposeString(spelling);
-		clang_disposeString(typeSpelling);
-		clang_disposeString(usr);
-		clang_disposeString(displayName);
+		std::cout << wrapper.debug_description() << std::endl;
 
 		return CXChildVisit_Recurse;
 	}
