285b53f927d4aa989420c762242de3e024573601
Grayson <info@fromconcentratesoftware.com>
2018-03-08 22:22:34 -0500

    Introduce basic means of testing arbitrary files

---
+++ tests/tests/cases/enum.hpp
@@ -0,0 +1,13 @@
+//
+//  enum.h
+//  tests
+//
+//  Created by Grayson Hansard on 3/8/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+enum Foo {
+	bar,
+	baz,
+	quux,
+}
---
+++ tests/tests/enum_tests.cpp
@@ -0,0 +1,14 @@
+//
+//  enum_tests.cpp
+//  tests
+//
+//  Created by Grayson Hansard on 3/8/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+#include "catch.hpp"
+#include <deduction/deductionlib.hpp>
+
+TEST_CASE("simple enum", "[enum]") {
+	REQUIRE(deduction::parse("cases/enum.hpp"));
+}
