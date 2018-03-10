b0ba05ee5c2f4a7807a9caa062e1193a4cd4d660
Grayson <info@fromconcentratesoftware.com>
2018-03-10 13:23:34 -0500

    Add function type

---
+++ library/deductionlib/function.cpp
@@ -0,0 +1,9 @@
+//
+//  function.cpp
+//  deductionlib
+//
+//  Created by Grayson Hansard on 3/10/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+#include "function.hpp"
---
+++ library/deductionlib/function.hpp
@@ -0,0 +1,40 @@
+//
+//  function.hpp
+//  deductionlib
+//
+//  Created by Grayson Hansard on 3/10/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace deduction {
+	struct function {
+		struct parameter {
+			parameter() = default;
+			parameter(std::string name, std::string type)
+				: name { std::move(name) }
+				, type { std::move(type) }
+			{}
+
+			std::string name;
+			std::string type;
+		};
+
+		function() = default;
+		function(std::string name, std::string fullName, std::vector<parameter> parameters, std::string returnType)
+			: name { std::move(name) }
+			, full_name { std::move(fullName) }
+			, parameters { std::move(parameters) }
+			, return_type { std::move(returnType) }
+		{};
+
+		std::string name;
+		std::string full_name;
+		std::vector<parameter> parameters;
+		std::string return_type;
+	};
+}
