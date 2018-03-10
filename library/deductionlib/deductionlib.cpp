70f52361956aa1417a07382124e3bb0b4866e745
Grayson <info@fromconcentratesoftware.com>
2018-03-10 13:24:30 -0500

    Parse functions

--- library/deductionlib/deductionlib.cpp
+++ library/deductionlib/deductionlib.cpp
@@ -15,6 +15,7 @@
 #include "clang-utility.hpp"
 #include "cursor.hpp"
 #include "enumeration.hpp"
+#include "function.hpp"
 
 namespace {
 	using namespace deduction;
@@ -26,6 +27,20 @@ namespace {
 		return CXChildVisit_Continue;
 	}
 
+	CXChildVisitResult visit_parameters(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+		auto parameters = reinterpret_cast<std::vector<function::parameter> *>(client_data);
+		parameters->emplace_back(get_name(cursor), get_type_name(cursor));
+		return CXChildVisit_Continue;
+	}
+
+	function map_to_function(CXCursor cursor) {
+		std::vector<function::parameter> parameters;
+		clang_visitChildren(cursor, visit_parameters, &parameters);
+		auto type = clang_getCursorType(cursor);
+		auto resultType = clang_getResultType(type);
+		return { get_name(cursor), qualify_name(cursor), parameters, map(clang_getTypeSpelling(resultType)) };
+	}
+
 	CXChildVisitResult visit(CXCursor cursor, CXCursor parent, CXClientData client_data) {
 		auto items = reinterpret_cast<std::vector<parse_result::item_type> *>(client_data);
 
@@ -33,11 +48,20 @@ namespace {
 		if (wrapper.is_invalid() || !wrapper.is_visible() || !wrapper.is_available() || !wrapper.is_in_main_file() || wrapper.is_preprocessing())
 			return CXChildVisit_Continue;
 
-		if (wrapper.kind() == CXCursor_EnumDecl) {
+		switch (wrapper.kind())
+		{
+		case CXCursor_EnumDecl: {
 			std::vector<enumeration::case_label> caseLabels;
 			clang_visitChildren(cursor, visit_enum_cases, &caseLabels);
 			items->push_back(enumeration { wrapper.name(), wrapper.full_name(), std::move(caseLabels) });
 			return CXChildVisit_Continue;
+		}
+		case CXCursor_FunctionDecl: {
+			items->emplace_back(map_to_function(cursor));
+			return CXChildVisit_Continue;
+		}
+		default:
+			std::cerr << "Unhandled cursor kind: " << wrapper.kind() << " (" << wrapper.type_name() << ")" << std::endl;
 		}
 
 		return CXChildVisit_Recurse;
