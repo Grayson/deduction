95722ea2376051df0b7d326846d0a5620fd805fe
Grayson <info@fromconcentratesoftware.com>
2018-03-26 22:42:30 -0400

    Add enumeration conversion

---
+++ deductionjson/deductionjson/enumeration.cpp
@@ -0,0 +1,44 @@
+//
+//  enumeration.cpp
+//  deductionjson
+//
+//  Created by Grayson Hansard on 3/26/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+#include <deduction/enumeration.hpp>
+#include "json.hpp"
+
+namespace deduction {
+	using nlohmann::json;
+
+	static char const * const NameLabel = "name";
+	static char const * const FullNameLabel = "full_name";
+	static char const * const CasesLabel = "cases";
+
+	void to_json(json & j, const enumeration::case_label & label) {
+		j = json {
+			{ NameLabel, label.name },
+			{ FullNameLabel, label.full_name }
+		};
+	}
+
+	void from_json(const json & j, enumeration::case_label & label) {
+		label.name = j.at(NameLabel).get<std::string>();
+		label.full_name = j.at(FullNameLabel).get<std::string>();
+	}
+
+	void to_json(json & j, const enumeration & enumeration) {
+		j = json {
+			{ NameLabel, enumeration.name },
+			{ FullNameLabel, enumeration.full_name },
+			{ CasesLabel, enumeration.case_labels }
+		};
+	}
+
+	void from_json(const json & j, enumeration & enumeration) {
+		enumeration.name = j.at(NameLabel).get<std::string>();
+		enumeration.full_name = j.at(FullNameLabel).get<std::string>();
+		enumeration.case_labels = j.at(CasesLabel).get<std::vector<enumeration::case_label>>();
+	}
+}
