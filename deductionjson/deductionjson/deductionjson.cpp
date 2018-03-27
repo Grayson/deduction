1bd30ee7fa9e001045f1e45cb11181117c8b6b8f
Grayson <info@fromconcentratesoftware.com>
2018-03-26 22:33:24 -0400

    Create `deductionjson` lib to encapsulate json conversions

---
+++ deductionjson/deductionjson/deductionjson.cpp
@@ -0,0 +1,9 @@
+//
+//  deductionjson.cpp
+//  deductionjson
+//
+//  Created by Grayson Hansard on 3/26/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+#include "deductionjson.hpp"
---
+++ deductionjson/deductionjson/deductionjson.hpp
@@ -0,0 +1,14 @@
+//
+//  deductionjson.hpp
+//  deductionjson
+//
+//  Created by Grayson Hansard on 3/26/18.
+//  Copyright © 2018 From Concentrate Software. All rights reserved.
+//
+
+#pragma once
+
+#include <deduction/deductionlib.hpp>
+#include <deduction/variant.hpp>
+
+
