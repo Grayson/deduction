20c6e85a3bfd3ee193fc50fe21ba7e4c2075da87
Grayson <info@fromconcentratesoftware.com>
2018-04-16 18:41:45 -0400

    Add json tests for enumerations and roundtrip variables

--- tests/src/enum_tests.cpp
+++ tests/src/enum_tests.cpp
@@ -9,6 +9,9 @@
 #include "catch.hpp"
 #include <deduction/deduction.hpp>
 #include <deduction/datatypes/variant.hpp>
+#include <deduction/deductionjson.hpp>
+#include <deduction/json/json.hpp>
+#include <deduction/json/parse-result.hpp>
 
 TEST_CASE("simple enum", "[enum]") {
 	auto const result = deduction::parse("../cases/enum.hpp");
@@ -31,6 +34,30 @@ TEST_CASE("enum class", "[enum]") {
 	REQUIRE(result.items.size() == 1);
 
 	auto const & en = mpark::get<deduction::enumeration>(result.items[0]);
+	REQUIRE(en.name == "Foo");
+
+	REQUIRE(en.case_labels[0].name == "bar");
+	REQUIRE(en.case_labels[1].name == "baz");
+	REQUIRE(en.case_labels[2].name == "quux");
+
+	REQUIRE(en.case_labels[0].full_name == "Foo::bar");
+	REQUIRE(en.case_labels[1].full_name == "Foo::baz");
+	REQUIRE(en.case_labels[2].full_name == "Foo::quux");
+}
+
+TEST_CASE("enum->json", "[json/enum]") {
+	auto const result = deduction::parse("../cases/enum-class.hpp");
+	auto const jsonString = deduction::convert_parsed_result_to_json(result);
+	char const * const expectation = R"json({"items":[{"_type":"enumeration","cases":[{"full_name":"Foo::bar","name":"bar"},{"full_name":"Foo::baz","name":"baz"},{"full_name":"Foo::quux","name":"quux"}],"full_name":"Foo","name":"Foo"}]})json";
+	REQUIRE(jsonString == expectation);
+}
+
+TEST_CASE("json->enum", "[json/enum]") {
+	auto const json = nlohmann::json::parse(R"json({"items":[{"_type":"enumeration","cases":[{"full_name":"Foo::bar","name":"bar"},{"full_name":"Foo::baz","name":"baz"},{"full_name":"Foo::quux","name":"quux"}],"full_name":"Foo","name":"Foo"}]})json");
+	auto const results = json.at("items").get<deduction::parse_result>();
+	REQUIRE(results.items.size() == 1);
+
+	auto const & en = mpark::get<deduction::enumeration>(results.items[0]);
 	REQUIRE(en.name == "Foo");
 
 	REQUIRE(en.case_labels[0].name == "bar");
--- tests/src/variables-json-test.cpp
+++ tests/src/variables-json-test.cpp
@@ -8,6 +8,8 @@
 
 #include "catch.hpp"
 #include <deduction/deductionjson.hpp>
+#include <deduction/json/json.hpp>
+#include <deduction/json/parse-result.hpp>
 
 char const * const JsonExpectation = R"test({
 "items": [
@@ -27,10 +29,34 @@ char const * const JsonExpectation = R"test({
 "type": "void *",
 "type_without_qualifiers": "void *"
 }
-]
+],
+"metadata": {
+"file": "../cases/variables.hpp",
+"version": "0.0.1"
+}
 })test";
 
-TEST_CASE("json -> variables", "[json]") {
+TEST_CASE("variables -> json", "[json/variable]") {
 	auto const json = deduction::convert_header_to_json("../cases/variables.hpp");
 	REQUIRE(json == JsonExpectation);
+}
+
+TEST_CASE("json -> variables", "[json/variable]") {
+	auto const json = nlohmann::json::parse(JsonExpectation);
+	auto const results = json.at("items").get<deduction::parse_result>();
+	REQUIRE(results.items.size() == 2);
+
+	auto const & foo = mpark::get<deduction::variable>(results.items[0]);
+	REQUIRE(foo.name == "foo");
+	REQUIRE(foo.type == "const int");
+	REQUIRE(foo.type_without_qualifiers == "int");
+	REQUIRE(foo.is_extern == true);
+	REQUIRE(foo.is_mutable == false);
+
+	auto const & bar = mpark::get<deduction::variable>(results.items[1]);
+	REQUIRE(bar.name == "bar");
+	REQUIRE(bar.type == "void *");
+	REQUIRE(bar.type_without_qualifiers == "void *");
+	REQUIRE(bar.is_extern == true);
+	REQUIRE(bar.is_mutable == true);
 }
